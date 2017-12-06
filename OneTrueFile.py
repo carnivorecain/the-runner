3270ff200fcb996a92ee6e654ac8c64b026189a6
carnivorecain <34097980+carnivorecain@users.noreply.github.com>
2017-12-06 18:53:03 +0000

    added 1st building as runway

---
+++ OneTrueFile.py
@@ -0,0 +1,388 @@
+
+import pygame
+import math
+import random
+import sys
+from pygame.locals import *
+
+WIDTH = 800
+HEIGHT = 600
+
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+RED   = (255,   0,   0)
+GREEN = (  0, 255,   0)
+BLUE  = (  0,   0, 255)
+
+pygame.mixer.pre_init(44100, -16, 2, 2048) # connects audio
+pygame.init()
+pygame.font.init
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+
+pygame.display.set_caption('THAT WHICH RUNS DOES NOT FALL')
+
+clock = pygame.time.Clock()
+
+# Global Attributes 
+##################################
+class Data():
+    currentScreen = 0 # 0 is Start Screen, 1 is Playing, 2 is Game Over, 3 is Paused (Move paused to StartGame?)
+    isGameOver = False
+
+    Buildings = []
+    robotYOrgin = 0 # JASON HERE 
+    Y_Change = 0
+    time = 0
+    Jump_Velocity = 25
+    
+    T = 0
+    Metres = 0
+    Building_Speed = 600
+    Gravity = -75
+    Highscore = 0 
+
+    MaxBuildingHeightDifference = 200
+#KEY FUNCTIONS
+    ############################################################
+def rotate(lst): # rotates the buildings via a list
+    lst[:] = lst[1:] + [lst[0]]
+
+def getImage(path): # the point of this is?
+    image = pygame.image.load(path)
+    return image
+    global _image_library
+    image = _image_library.get(path)
+    if image == None:
+            canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+            image = pygame.image.load(canonicalized_path)
+            _image_library[path] = image
+    return image
+
+def playSound(filename): # plays the sfx
+    soundObj = pygame.mixer.Sound(filename)
+    soundObj.play()
+
+def playMusic(filename): # plays the music soundtrack
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+    
+class Random(): # randomixer for building proportions
+    def Height():
+        return random.randint(1,6)*45 # may want to adjust numbers as there are times can't make the jump
+
+    def Width():
+        return random.randint(8,15)*40
+
+    def Gap():
+       return random.randint(10,20)*5
+
+    def YPos():
+        return random.randint(20,55)*10
+
+# Visible Objects
+############################################################
+
+class Background(pygame.sprite.Sprite): # class for Background image
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        self.image = pygame.image.load(image_file) # loading image
+        self.rect = self.image.get_rect() # background rect
+        self.rect.left, self.rect.top = location # alignment? position?
+
+class Text():
+    def __init__(self, text, fnt, size, xcoord, ycoord,color):
+        self.text = text
+        self.fnt = fnt
+        self.size = size
+        self.xcoord = xcoord
+        self.ycoord = ycoord
+        self.color = color
+        
+    def text_objects(self,font): # making a space for text?
+        textSurface = font.render(self.text, True, self.color)
+        return textSurface, textSurface.get_rect()
+    
+    def Write(self):# assuming writing for text need to explain?
+        Texty = pygame.font.Font(self.fnt,self.size)
+        TextSurf, TextRect = self.text_objects(Texty)
+        TextRect.topright = ((self.xcoord,self.ycoord))
+        screen.blit(TextSurf,TextRect)
+
+
+class Building(pygame.sprite.Sprite): # Class for building
+    def __init__(self, x, y, width, height, color):
+        super().__init__()# calling sprite init
+        self.color = color
+        self.gap = Random.Gap() # assigns random gap size
+
+        self.name = 0 # the point of this? it does no change when commeted out although may not make a list properly  
+
+        self.tileStart = 0 # placement of start of tile?
+        self.tileWidth = 40 #width of tile what precisly is a tile?
+        #if width > self.tileWidth:
+        self.image = pygame.Surface([width, height]) # christine found a stack overflow page for this 
+        while self.tileStart < width:
+            if self.tileStart == 0:
+                self.image.blit(getImage('building_left.png'), (self.tileStart, 0))
+            elif self.tileStart > width - self.tileWidth - 1:
+                self.image.blit(getImage('building_right.png'), (self.tileStart, 0))
+            else:
+                self.image.blit(getImage('building_center.png'), (self.tileStart, 0))
+            self.tileStart += self.tileWidth
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    
+    def CrashRect(self): # rectangle for collisions 
+        return pygame.Rect(self.rect.x, self.rect.y, 10 ,self.rect.height)
+    
+    # def ShowCrashRect(self):
+    #     pygame.draw.rect(screen,BLACK,self.CrashRect())
+
+class Robot(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, color): # standard construction of robot 
+        super().__init__()
+        self.rect = pygame.Rect(x, y, height, width)
+        self.color = color
+        self.Initial_Velocity = 0.0 
+
+        self.runImages = [] # list for the running animation 
+        self.runImages.append(getImage('run_1.png'))
+        self.runImages.append(getImage('run_2.png'))
+        self.runImages.append(getImage('run_3.png'))
+        self.runImages.append(getImage('run_2.png'))
+
+        self.jumpImages = [] # list for the jumping animation
+        self.jumpImages.append(getImage('jump_1.png'))
+        self.jumpImages.append(getImage('jump_2.png'))
+        self.jumpImages.append(getImage('jump_3.png'))
+        self.jumpImages.append(getImage('jump_4.png'))
+        self.jumpImages.append(getImage('jump_5.png'))
+        self.jumpImages.append(getImage('jump_6.png'))
+        self.jumpImages.append(getImage('jump_7.png'))
+        self.jumpImages.append(getImage('jump_8.png'))
+        
+        self.index = 0
+        self.image = self.runImages[self.index]
+
+    def update(self):
+        '''This method iterates through the elements inside self.images and 
+        displays the next one each tick. For a slower animation, you may want to 
+        consider using a timer of some sort so it updates slower.'''
+
+        imageList = self.runImages
+        if self.Initial_Velocity > 0 :
+            imageList = self.jumpImages
+            # when you jump, you need to reset the index to 0 # 
+        
+        imageTicks = 5 # number of loops each animation frame shows for
+        self.index += 1
+        if self.index >= len(imageList)*imageTicks:
+            self.index = 0
+        imageCount = int(self.index/imageTicks)
+        self.image = imageList[imageCount]
+
+        if self.rect.width < self.image.get_rect().width:
+            self.image = pygame.transform.scale(self.image, (self.rect.width, self.rect.height))
+
+        if self.rect.y>= 600:# this checks for falling game over
+            print("robot has fallen down")
+            showGameOver()            
+
+def showGameOver(): # sprites for death and stops map
+    # stop map
+    # death animation sequence
+    print("GAME OVER")
+    Data.currentScreen = 2
+    Data.isGameOver = True
+
+def resetVariables():# no touch
+    running = True
+    Data.Building_Speed = 600
+    Data.Metres = 0
+    Data.robotYOrign = 0 # dont touch 
+
+    
+                               
+def runGame():
+    #resetVariables() not needed here it is in the reset loop 
+    
+
+    # calculation functions
+    ############################################################
+
+
+    def calcBuildingsPos(tick):
+        # move first building to the back if it has gone offscreen
+        firstBuilding = Data.Buildings[0]
+        if firstBuilding.rect.x < -firstBuilding.rect.width: 
+            firstBuilding.gap = Random.Gap()
+            rotate(Data.Buildings)  #moves rects to the back of the queue
+
+        previousBuilding = Data.Buildings[0]
+        for building in Data.Buildings:
+            # print("building " + str(building.name) + " at " + str(building.rect))
+            if building == previousBuilding:
+                building.rect.x -= Data.Building_Speed * tick / 1000.0
+            else:
+                building.rect.x = previousBuilding.rect.x + previousBuilding.rect.width + previousBuilding.gap
+            previousBuilding = building
+
+    def collision2() :
+        for building in Data.Buildings:
+            if robot.rect.colliderect(building.CrashRect()) :        
+                print("hits wall game over")
+                robot.rect.x = building.rect.x - building.tileWidth
+                Data.Building_Speed = 0
+                # death animation
+                    # death animation must delete robot
+                    #and also run showgameover
+                #showGameOver()
+            elif robot.rect.colliderect(building.rect) and robot.rect.y:
+                print("collision with building " + str(building.name))
+                Data.robotYOrgin = robot.rect.y ########################
+                robot.rect.y = building.rect.y-robot.rect.height
+                Data.T = 0
+                robot.Initial_Velocity = 0
+
+    def calcRobotPos(tick):
+        Data.T += tick / 1000.0
+        print(Data.robotYOrgin)
+        #robotYOrig = robot.rect.y  # makes variable for robot intial Y postion per tick  # JASON CHANGES EQUATION 
+        Data.Y_Change = (robot.Initial_Velocity) + (Data.Gravity*Data.T)
+        # print("Data T:" + str(Data.T) + ", y-change:" + str(Data.Y_Change))
+        robot.rect.y -= Data.Y_Change
+        # print("Robot:" + str(robot.rect.y))
+
+
+
+        
+
+
+    # Object creation
+    ############################################################
+    background = Background('background.png', [0,0])
+
+    building = Building(0, 400, 1500, 600, BLACK) #hardcode first building with runway width
+    Data.Buildings.append(building)        
+    lastY = building.rect.y #for calculating if building height is too high to jump
+    for i in range(0,4):
+        lastBuilding = Data.Buildings[-1] #get last building we made     
+        startX = lastBuilding.rect.x + lastBuilding.rect.width + Random.Gap() #new X position is where last building starts 
+
+
+        building = Building(startX + Random.Gap(), Random.YPos(), Random.Width(), 600, BLACK) #creates the Buildings
+        #check if building can be jumped to
+        if building.rect.y < lastY - Data.MaxBuildingHeightDifference:
+            building.rect.y = lastY - Data.MaxBuildingHeightDifference
+        lastY = building.rect.y
+        building.name = i
+        Data.Buildings.append(building)
+
+    building_group = pygame.sprite.Group(Data.Buildings)
+
+
+    #robot = Robot(5,0,60,60,BLUE)
+    robot = Robot(5, lastBuilding.rect.y-60,60,60,BLUE)
+
+    robot_group = pygame.sprite.Group(robot)
+
+    while not Data.isGameOver:
+        print("inside loop")
+        
+
+        tick = clock.tick(60) # gives time since last frame in ms
+  
+
+        calcBuildingsPos(tick) # calculate positions using tick to account for processing lag
+
+        calcRobotPos(tick) # same as above
+
+        collision2()
+        
+        
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if Data.T < 0.0001 and event.type == pygame.KEYDOWN and event.key == pygame.K_UP: #makes the robot jump
+                print("jump")
+                playSound("Jump.wav")
+                robot.Initial_Velocity = Data.Jump_Velocity
+            if event.type == pygame.KEYDOWN and (event.key == pygame.K_p or event.key == pygame.K_ESCAPE):
+                Data.Building_Speed = 0
+                Data.currentScreen = 3
+            
+                 
+        screen.fill(BLACK)
+        screen.blit(background.image, background.rect)
+
+        
+        building_group.update()
+        building_group.draw(screen)
+        robot_group.update()
+        robot_group.draw(screen)
+        
+        Data.Metres += 1 # score section
+        if Data.Highscore < Data.Metres :
+            Data.Highscore = Data.Metres
+        Score = Text("Highscore: " + str(Data.Highscore) + "m  / "+"Current Run: " +
+                     str(Data.Metres) + "m",'freesansbold.ttf',30,750,50,WHITE)  #prints the score on the screen
+        Score.Write()
+                
+        pygame.display.flip()
+
+    # TODO : make sure buildings and everything resets properly
+    building_group.empty()
+    robot_group.empty()
+    Data.Buildings = []
+
+# Run loop(s)
+#################################################################    
+    
+playMusic("MortalMachine.ogg")
+
+running = True
+
+flicker = 0.0 # flicker? what is this 
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+            pygame.quit()
+            
+        if event.type == pygame.KEYDOWN and (event.key == pygame.K_SPACE or event.key == pygame.K_r) :
+            Data.isGameOver = False
+            resetVariables()
+            runGame()
+            
+            # Data.currentScreen = 1
+
+    if Data.currentScreen == 0:
+        flicker += clock.tick(60) # gives time since last frame in ms
+        backgroundImage = 'Start_1.png'
+        if flicker > 1000:
+            backgroundImage = 'Start_2.png'
+        if flicker > 2000:
+            flicker = 0
+        startScreen = Background(backgroundImage, [0,0])
+        screen.fill(BLACK)
+        screen.blit(startScreen.image, startScreen.rect)
+        pygame.display.flip()
+
+    elif Data.currentScreen == 2:
+        screen.fill(WHITE)
+        gameover = Text("Game Over. Q to quit R to restart",'freesansbold.ttf',30,600,300,BLACK)
+        gameover.Write()
+        pygame.display.flip()
+        
+    elif Data.currentScreen == 3:
+        screen.fill(WHITE)
+        pausescreen = Text("Paused. Q to quite or P to continue",'freesansbold.ttf',30,600,300,BLACK)
+        pausescreen.Write()
+        pygame.display.flip()
+
+pygame.quit()
+
+# gameover = Text("Game Over. Q to quit R to restart",'freesansbold.ttf',30,400,300,BLACK)
